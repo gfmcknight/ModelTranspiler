81b48ecd16179a60f8efa962bc73f16e1f4bfddf
Graham McKnight <graham.mcknight2@gmail.com>
2019-04-07 20:25:25 -0700

    Initial commit
    Write a rudimentary set of functions to convert C# classes to Typescript
    classes.

---
+++ ModelTranspiler/ClassTranspiler.fs
@@ -0,0 +1,42 @@
+﻿module ClassTranspiler
+
+open System
+open Microsoft.CodeAnalysis.CSharp
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open System.Reflection
+open System.Linq.Expressions
+
+let convertType csharpType =
+    csharpType
+
+let convertProperty (propertyDeclaration: PropertyDeclarationSyntax) = 
+    let propertyType = convertType (propertyDeclaration.Type.ToString()) in
+    let propertyName = propertyDeclaration.Identifier.ToString() in
+    let fieldDecl = sprintf "private _%s : %s;\n" propertyName propertyType in
+
+    let accessors = Seq.cast<AccessorDeclarationSyntax> 
+                        (Seq.filter (fun i -> TypeExtensions.IsInstanceOfType(typeof<AccessorDeclarationSyntax>, i))
+                           (Seq.cast<SyntaxNode> (propertyDeclaration.DescendantNodes())))
+
+
+    let convertAccessor (accessor: AccessorDeclarationSyntax) =
+        match (accessor.ToString()) with
+            | "get;" -> sprintf "get %s() : %s\n{\nreturn this._%s;\n}\n" propertyName propertyType propertyName
+            | "set;" -> sprintf "set %s(new%s : %s)\n{\nthis._%s = new%s;\n}\n"
+                                propertyName propertyName propertyType propertyName propertyName
+            | _ -> ""
+    in
+    Seq.fold (+) fieldDecl (Seq.map convertAccessor accessors)
+   
+
+    
+
+let convertClass (classDeclaration : ClassDeclarationSyntax) = 
+    let properties = Seq.cast<PropertyDeclarationSyntax> (
+                         Seq.filter (fun i -> TypeExtensions.IsInstanceOfType(typeof<PropertyDeclarationSyntax>, i))
+                             (Seq.cast<SyntaxNode> (classDeclaration.DescendantNodes())))
+    in
+    let convertedProperties = Seq.fold (+) "" (Seq.map (fun prop -> (convertProperty prop) + "\n") properties)
+    in
+    "class " + classDeclaration.Identifier.ToString() + " {\n" + convertedProperties + "}"
--- ModelTranspiler/Program.fs
+++ ModelTranspiler/Program.fs
@@ -1,8 +1,43 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open Microsoft.CodeAnalysis.CSharp
+open Microsoft.CodeAnalysis
+open System.Reflection
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open ClassTranspiler
 
-open System
+let programText = @"using System;
+using System.Collections.Generic;
+using System.Text;
+ 
+namespace HelloWorld
+{
+    class Program
+    {
+        public int MyNumber { get; private set; }
+        public double MyOtherProp { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello, World!"");
+        }
+    }
+}"
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let tree = CSharpSyntaxTree.ParseText(programText)
+let root = tree.GetCompilationUnitRoot()
+
+let compilation = CSharpCompilation.Create("HelloWorld")
+                    .AddReferences(MetadataReference.CreateFromFile(typeof<string>.Assembly.Location))
+                    .AddSyntaxTrees(tree)
+
+let model = compilation.GetSemanticModel(tree, false)
+let myClasses = Seq.filter (fun i -> TypeExtensions.IsInstanceOfType(typeof<ClassDeclarationSyntax>, i))
+                           (Seq.cast<SyntaxNode> (root.DescendantNodes()))
+
+let myClassesAsClasses = Seq.cast<ClassDeclarationSyntax> myClasses
+
+
+printf "%s" (convertClass (Seq.head myClassesAsClasses))
+for i in root.DescendantNodesAndSelf() do
+    printf "\n\n%s ###::: %s\n" (i.GetType().Name) (i.ToString())
+
+let line = Console.ReadLine()
